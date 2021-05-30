0c8563a4a809cb258d956c7a8fd069f7061507c4
andrii-pulya <andrey.predein@gmail.com>
2021-05-30 15:21:17 +0300

    ready except styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,4 @@
 import '../sass/main.scss';
-
-// 1) зарегистрироваться и получить ключ +
-// 2) создать инпут и достучаться до него +
-// 3) создать Галерея изображений и достучаться до него +
-// 4) создать Карточка изображения шаблон + / -
-// 5) добавить Material Icons +
-// 6) создать кнопку Load more и достучаться до нее +
-// 7)
-
 import pictureTemplate from '../templates/picture-template.hbs';
 import debounce from 'lodash.debounce';
 import pictureFind from './services/apiService.js';
@@ -15,24 +6,41 @@ import pictureFind from './services/apiService.js';
 const refs = {
   searchField: document.querySelector('.search-form'),
   galleryContainer: document.querySelector('.gallery'),
-  loadMoreBtn: document.querySelector('load-more-btn'),
+  loadMoreBtn: document.querySelector('.load-more-btn'),
 };
 
-// refs.loadMoreBtn.scrollIntoView({
-//   behavior: 'smooth',
-//   block: 'end',
-// });
-
-refs.searchField.addEventListener(
-  'input',
-  debounce(e => {
-    pictureFind(e.target.value)
-      .then(response => {
+window.onload = () => {
+  refs.searchField.addEventListener(
+    'input',
+    debounce(e => {
+      if (e.target.value === '') {
         refs.galleryContainer.innerHTML = '';
+      } else {
+        pictureFind(e.target.value)
+          .then(response => {
+            refs.searchField.value = e.target.value;
+            refs.galleryContainer.innerHTML = '';
+            const markupPictures = pictureTemplate(response.data.hits);
+            refs.galleryContainer.insertAdjacentHTML('beforeend', markupPictures);
+          })
+          .catch(error => console.log(error));
+      }
+    }, 500),
+  );
+
+  refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+  function onLoadMore(e) {
+    e.preventDefault();
+    pictureFind(refs.searchField.value)
+      .then(response => {
         const markupPictures = pictureTemplate(response.data.hits);
-        // console.log(markupPictures);
         refs.galleryContainer.insertAdjacentHTML('beforeend', markupPictures);
+        refs.loadMoreBtn.scrollIntoView({
+          behavior: 'smooth',
+          block: 'end',
+        });
       })
       .catch(error => console.log(error));
-  }, 500),
-);
+  }
+};
--- src/js/services/apiService.js
+++ src/js/services/apiService.js
@@ -1,45 +1,15 @@
 import axios from 'axios';
 
-// const pictureFind = async pictureName => {
-//   axios.defaults.baseURL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=';
-//   const myKey = '21807321-d1b9b9077da7f78b4c19cddb8';
-
-//   try {
-//     let pageNumber = 1;
-//     const request = await axios.get(`${pictureName}&page=${pageNumber}&per_page=12&key=${myKey}`);
-//       return request.json().then(data => console.log(data));
-
-//   } catch (error) {
-//     return error;
-//   }
-// };
-
-// const pictureFind = async pictureName => {
-//   axios.defaults.baseURL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal';
-//   const myKey = '21807321-d1b9b9077da7f78b4c19cddb8';
-//   try {
-//     let pageNumber = 1;
-//     const request = await axios.get(
-//       `&q=${pictureName}&page=${pageNumber}&per_page=12&key=${myKey}`,
-//     );
-//     console.log(request);
-//     return request;
-//   } catch (error) {
-//     return error;
-//   }
-// };
-
-// export default pictureFind;
+let pageNumber = 1;
 
 const pictureFind = async pictureName => {
   axios.defaults.baseURL = 'https://pixabay.com/api/';
   const myKey = '21807321-d1b9b9077da7f78b4c19cddb8';
   try {
-    let pageNumber = 1;
     const request = await axios.get(
       `?key=${myKey}&q=${pictureName}&page=${pageNumber}&per_page=12&image_type=photo`,
     );
-    console.log(request);
+    pageNumber += 1;
     return request;
   } catch (error) {
     return error;
@@ -47,20 +17,3 @@ const pictureFind = async pictureName => {
 };
 
 export default pictureFind;
-
-// export default function getData(searchValue, pageNumber) {
-//   return axios(
-//     `${baseUrl}?image_type=photo&orientation=horizontal&q=${searchValue}&page=${pageNumber}&per_page=12&key=${key}`,
-//   ).then(({ data }) => data.hits);
-// }
-
-// export default async (name, page) => {
-//   try {
-//     const picturesList = await fetch(
-//       `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${name}&page=${page}&per_page=9&key=18996583-5f88ddbbd0a62c224fff1ccf9`,
-//     );
-//     return picturesList.json();
-//   } catch (error) {
-//     return error;
-//   }
-// };
